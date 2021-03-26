2d57c51ee5b5f2ba8822b0311de70dfa4a2c5226
morqan <qurbanbeyli.murad@gmail.com>
2021-03-26 18:07:38 +0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "@material-ui/core";
+
+const Index = () => {
+    return (
+        <div>
+            home page
+            <Button>start</Button>
+        </div>
+    );
+};
+
+export default Index;
